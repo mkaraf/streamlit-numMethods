2060b2648baac345222d359cf5e953d9984fadc0
mkaraf <karafiat.michal@gmail.com>
2021-05-04 18:39:46 +0200

    update main

--- main.py
+++ main.py
@@ -39,6 +39,7 @@ s9 = "y' - y = x"
 
 EQUATIONS = [s1, s2, s3, s4, s5, s6, s7, s8, s9]
 MODELS = [f1, f2, f3, f4, f5, f6, f7, f8, f9]
+# METHODS = ["Euer","Heun","RK2","RK3","RK4","AB2","AB3","AB4","ABM PRED-COR","RKF45"]
 
 
 def getDataFrame(f, a, b, n, y0):
@@ -54,35 +55,35 @@ def getDataFrame(f, a, b, n, y0):
     heun_exec_time = perf_counter() - start_time_p2
 
     start_time_p3 = perf_counter()
-    rk2 = numMethods.rk2(f, y0, x, h)
+    rk2 = np.array(numMethods.rkx(f, [y0], x, h, 2)).flatten()
     rk2_exec_time = perf_counter() - start_time_p3
 
     start_time_p4 = perf_counter()
-    rk3 = numMethods.rk3(f, y0, x, h)
+    rk3 = np.array(numMethods.rkx(f, [y0], x, h, 3)).flatten()
     rk3_exec_time = perf_counter() - start_time_p4
 
     start_time_p5 = perf_counter()
-    rk4 = numMethods.rk4(f, y0, x, h)
+    rk4 = np.array(numMethods.rkx(f, [y0], x, h, 4)).flatten()
     rk4_exec_time = perf_counter() - start_time_p5
 
     start_time_p6 = perf_counter()
-    ab2 = numMethods.ab2(f, y0, x, h)
+    ab2 = numMethods.abx(f, y0, x, h, 2)
     ab2_exec_time = perf_counter() - start_time_p6
 
     start_time_p7 = perf_counter()
-    ab3 = numMethods.ab3(f, y0, x, h)
+    ab3 = numMethods.abx(f, y0, x, h, 3)
     ab3_exec_time = perf_counter() - start_time_p7
 
     start_time_p8 = perf_counter()
-    ab4 = numMethods.ab4(f, y0, x, h)
+    ab4 = numMethods.abx(f, y0, x, h, 4)
     ab4_exec_time = perf_counter() - start_time_p8
 
     start_time_p9 = perf_counter()
-    rkf_x, rkf_y, e = numMethods.rkf45(f, y0, a, b, 1e-5, 0.25, 0.01)
+    rkf_x, rkf_y, rkf45_status = numMethods.rkf45(f, y0, a, b, 1*10**tol, h_max, h_min)
     rkf45_exec_time = perf_counter() - start_time_p9
 
     start_time_p10 = perf_counter()
-    abm4_pc = numMethods.ab4(f, y0, x, h)
+    abm4_pc = numMethods.abm4_pc(f, y0, x, h)
     stop_time = perf_counter()
     abm4_exec_time = stop_time - start_time_p10
 
@@ -96,12 +97,15 @@ def getDataFrame(f, a, b, n, y0):
     df_et.index = ['Euler', 'Heun', 'RK2', 'RK3', 'RK4', 'AB2', 'AB3', 'AB4', 'RKF45','ABM4_PC']
     df_et['method'] = df_et.index
 
-    df_rkf45 = pd.DataFrame({
-            'RKF45': rkf_y
-        },
-        index=rkf_x
-    )
-    df_rkf45.index.name = 'x'
+    if 0 == rkf45_status:
+        df_rkf45 = pd.DataFrame({
+                'RKF45': rkf_y
+            },
+            index=rkf_x
+        )
+        df_rkf45.index.name = 'x'
+    else:
+        df_rkf45 = 0
 
 
     # DATAFRAME
@@ -120,33 +124,40 @@ def getDataFrame(f, a, b, n, y0):
         index=x
     )
     df.index.name = 'x'
-    return df, df_rkf45, df_et
+    return df, df_rkf45, df_et, rkf45_status
 
 
-def getChart(df, df_rkf45):
-    source = df.reset_index().melt('x', var_name='method', value_name='y')
-    source_rkf45 = df_rkf45.reset_index().melt('x', var_name='method', value_name='y')
+def getChart(df, df_rkf45, rkf45_status):
+    source_1 = df.reset_index().melt('x', var_name='method', value_name='y')
+    if 0 == rkf45_status:
+        source_rkf45 = df_rkf45.reset_index().melt('x', var_name='method', value_name='y')
 
-    # # The basic line
-    # line2 = alt.Chart().mark_line(point=True).encode(
-    #     x='x:Q',
-    #     y='y:Q',
-    #     color='method:N'
-    # )
+        frames = [source_1, source_rkf45]
+        source = pd.concat(frames)
+    else:
+        source = source_1
 
-    # The basic line
-    line = alt.Chart().mark_line(point=True).encode(
-        x='x:Q',
-        y='y:Q',
-        color='method:N'
+
+    selection = alt.selection_multi(fields=['method'])
+    color = alt.condition(selection,
+                          alt.Color('method:N',legend=None),
+                          alt.value('lightgray'))
+
+    line_chart = alt.Chart(source,title="Results").mark_line(point=True).encode(
+        x=alt.X('x:Q', axis=alt.Axis(title='x [-]')),
+        y=alt.Y('y:Q', axis=alt.Axis(title='y [-]')),
+        color=color,
+        tooltip='Name:N'
+    ).properties(width=700, height=400).transform_filter(selection)#.interactive()
+
+    make_selector = alt.Chart(source).mark_rect().encode(
+        y=alt.Y('method:N', axis=alt.Axis(orient='right')),
+        color=color
+    ).add_selection(
+        selection
     )
 
-    # Put the five layers into a chart and bind the data
-    # chart = alt.layer(line, data=source, title="Result y'= ").interactive().properties(width=800, height=400)
-    chart_1 = alt.layer(line, data=source, title="Result y'= ")
-    chart_2 = alt.layer(line, data=source_rkf45, title="Result y'= ")
-    chart = (chart_1 + chart_2).interactive().properties(width=800, height=400)
-    return chart
+    return line_chart | make_selector
 
 
 def getChart2(df_et):
@@ -176,13 +187,16 @@ with sidebar:
     st.sidebar.header("User Input Parameters")
     equation = st.sidebar.selectbox("Select equation:", EQUATIONS)
 
+
     st.sidebar.markdown("***")
     st.sidebar.markdown("Initial conditions")
     y0 = st.sidebar.number_input("y0 = ", 0.0)
 
+    # check_boxes = [st.sidebar.checkbox(method, key=method) for method in METHODS]
+
     st.sidebar.markdown("***")
     st.sidebar.markdown("RKF45")
-    h_min, h_max = st.sidebar.slider("step interval:", 0.01, 1.0, (0.1, 0.75), 0.01)
+    h_min, h_max = st.sidebar.slider("step interval:", 0.001, 1.0, (0.001, 0.75), 0.001)
     tol = st.sidebar.slider("Tolerance: (1e(x)):", -12, -5, -7)
 
 
@@ -190,6 +204,10 @@ with features:
     st.write("Equation:")
     #st.latex(equation.replace('c', str(c)))
     st.latex(equation)
+
+    # st.write("Analytical solution:")
+    # x = np.linspace(-1000, 1000, n + 1)
+
     n = st.slider("Number of steps:", 5, 100, 10)
     a, b = st.slider("Interval:", -10.0, 10.0, (0.0, 5.0), 0.1)
 
@@ -205,9 +223,17 @@ with features:
 
 
 with interactive:
-    df, df_rkf45, df_et = getDataFrame(f, a, b, n, y0)
-    chart = getChart(df, df_rkf45)
+    df, df_rkf45, df_et, rkf45_status = getDataFrame(f, a, b, n, y0)
+    chart = getChart(df, df_rkf45, rkf45_status)
     chart2 = getChart2(df_et)
+
+    if -1 == rkf45_status:
+        st.markdown("Warning:")
+        st.markdown("RKF45 can't be solved with choose parameters, try change minimum of the interval or tolerance.")
+
+    # <span styl="color:red"> TEST </span>
+    # st.markdown(f'<span styl=color:red> TEST </span>',unsafe_allow_html=True)
     st.altair_chart(chart)
+    st.markdown("Note: By holding shift you can pick multiple choices in the legend")
     st.table(df)
     st.altair_chart(chart2)
