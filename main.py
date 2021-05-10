52d6602cac73447c7965360b19eaee8d3b3c3735
mkaraf <karafiat.michal@gmail.com>
2021-05-10 12:16:53 +0200

    update

--- main.py
+++ main.py
@@ -5,22 +5,29 @@ import numpy as np
 import streamlit as st
 import pandas as pd
 import altair as alt
-from scipy.integrate import odeint
 from functools import wraps
+# from scipy.integrate import odeint
+# from functools import wraps
 
+
+st.set_page_config(layout="wide")
+link = '[ODE Math Laboratory](http://calculuslab.deltacollege.edu/ODE/ODE-h.html)'
 
 METHODS = ["NONE", "Euler", "Heun", "Runge-Kutta 2", "Runge-Kutta 3", "Runge-Kutta 4", "Adams-Bashforth 2",
            "Adams-Bashforth 3", "Adams-Bashforth 4", "Adams-Bashforth-Moulton", "Runge-Kutta Fehlberg 45"]
-MODELS = [models.f0, models.f1, models.f2, models.f3, models.f4, models.f5]
-EQUATIONS = models.get_model_str()
 
-st.set_page_config(layout="wide")
+EQUATIONS = models.get_system_str()
+MODELS = [models.model_math_pendulum, models.model_do]
+ANALYTIC = [models.analytic_math_pendulum, models.get_analytical_real]
+
 
 analytic = pd.DataFrame(columns=['Analytical'])
+table = pd.DataFrame(columns=['Analytical','Euler', 'Heun', 'RK2', 'RK3', 'RK4', 'AB2', 'AB3', 'AB4', 'ABM4_PC'])
 numeric = pd.DataFrame(columns=['Euler', 'Heun', 'RK2', 'RK3', 'RK4', 'AB2', 'AB3', 'AB4', 'ABM4_PC'])
 rkf45 = pd.DataFrame(columns=['RKF45'])
 analytic.index.name = 'x'
 numeric.index.name = 'x'
+table.index.name = 'x'
 rkf45.index.name = 'x'
 first_run = True
 
@@ -119,7 +126,7 @@ def calc_numerical(model, a_min, b_max, init_con, num_steps, step_size, step_siz
     ab3 = np.array(numMethods.abx(model, init_con, x, step_size, 3))[:, 0]
     ab4 = np.array(numMethods.abx(model, init_con, x, step_size, 4))[:, 0]
     abm4_pc = np.array(numMethods.abm4_pc(model, init_con, x, step_size))[:, 0]
-    rkf_x, rkf_y, rkf45_stat = np.array(numMethods.rkf45(model, init_con, a, b, 1 * 10 ** tolerance,
+    rkf_x, rkf_y, rkf45_stat = np.array(numMethods.rkf45(model, init_con, a, b, tolerance,
                                                          step_size_max, step_size_min))
     rkf_y = np.array(rkf_y)[:, 0]
 
@@ -153,28 +160,65 @@ def calc_numerical(model, a_min, b_max, init_con, num_steps, step_size, step_siz
     return df_num_sol, df_rkf45, rkf45_status
 
 
-def calc_analytical(a_min, b_max, model, init_con):
-    number_of_steps = (b_max - a_min) / 0.01
-    x = np.linspace(a_min, b_max, round(number_of_steps + 1.0))
-    solution = odeint(flip(model), init_con, x)
-    pd_analytic = pd.DataFrame({'value': solution[:, 0]}, index=x)
+def get_table(num, an):
+    result = pd.concat([an, num], axis=1)
+    return result
+
+
+def calc_analytical_chart(a_min, b_max, analytic_formula, init_con, step):
+    number_of_steps = round((b_max - a_min) / step) + 1
+    x = np.linspace(a_min, b_max, number_of_steps)
+    # solution = odeint(flip(model), init_con, x)
+    solution = [0] * number_of_steps
+    for i in range(number_of_steps):
+        solution[i] = analytic_formula(x[i], init_con)
+    pd_analytic = pd.DataFrame({'Analytical': solution}, index=x)
     return pd_analytic
+
+
+def calc_truncation(vals):
+    last_row = vals.iloc[[-1]]
+    # st.write(last_row)
+
+    # DATAFRAME
+    df_global_trunc = pd.DataFrame({
+        'Euler': abs(last_row['Analytical'] - last_row['Euler']),
+        'Heun': abs(last_row['Analytical'] - last_row['Heun']),
+        'RK2': abs(last_row['Analytical'] - last_row['RK2']),
+        'RK3': abs(last_row['Analytical'] - last_row['RK3']),
+        'RK4': abs(last_row['Analytical'] - last_row['RK4']),
+        'AB2': abs(last_row['Analytical'] - last_row['AB2']),
+        'AB3': abs(last_row['Analytical'] - last_row['AB3']),
+        'AB4': abs(last_row['Analytical'] - last_row['AB4']),
+        'ABM4_PC': abs(last_row['Analytical'] - last_row['ABM4_PC'])
+    },
+        index=last_row.index
+    )
+    df_global_trunc.index.name = 'x'
+
+    bchart_source = df_global_trunc.reset_index().melt('x', var_name='method', value_name='y')
+    bars = alt.Chart(bchart_source, title='Global Truncation').mark_bar().encode(
+        x='y:Q',
+        y=alt.Y('method:N',sort=alt.EncodingSortField(field='y', order='ascending', op='sum'),),
+        color=alt.Color('method', legend=None)
+    ).properties(width=800, height=400)
+
+    st.altair_chart(bars)
+    return
 
 
 header = st.beta_container()
 sidebar = st.beta_container()
 equations = st.beta_container()
 left_ode, right_analytic = st.beta_columns(2)
+init_conditions = st.beta_container()
 
 control_panel = st.beta_container()
 start_point_left, start_point_right = st.beta_columns(2)
 request_analytical, request_numerical = st.beta_columns(2)
-calculations = st.spinner('Calculation in progress...')
-
-# features = st.beta_container()
-# interactive = st.beta_container()
-# table = st.beta_expander("Show me the data in the table")
-# about = st.beta_container()
+calculations = st.beta_container()
+about = st.beta_container()
+footer = st.beta_container()
 
 
 with header:
@@ -188,97 +232,100 @@ with sidebar:
     equation = st.sidebar.selectbox("Select equation:", EQUATIONS)
     id_mod = EQUATIONS.index(equation)
     f = MODELS[id_mod]
+    general_solution = ANALYTIC[id_mod]
 
-    # st.sidebar.markdown("***")
-    # st.sidebar.markdown("Constansts")
-    # c = [
-    #     st.sidebar.number_input("a0 = ", 0.0),
-    #     st.sidebar.number_input("a1 = ", 0.0),
-    #     st.sidebar.number_input("a3 = ", 0.0),
-    #     st.sidebar.number_input("a2 = ", 0.0)
-    # ]
-    # if id_mod < 4:
-    #     c = [
-    #         st.sidebar.number_input("a0 = ", 0.0),
-    #         st.sidebar.number_input("a1 = ", 0.0)
-    #     ]
-    # elif id_mod == 4:
-    #     c = [
-    #         st.sidebar.number_input("a0 = ", 0.0),
-    #         st.sidebar.number_input("a1 = ", 0.0),
-    #         st.sidebar.number_input("a2 = ", 0.0)
-    #     ]
-    # elif id_mod == 5:
-    #     c = [
-    #         st.sidebar.number_input("a0 = ", 0.0),
-    #         st.sidebar.number_input("a1 = ", 0.0),
-    #         st.sidebar.number_input("a2 = ", 0.0),
-    #         st.sidebar.number_input("a3 = ", 0.0)
-    #     ]
+    if id_mod == 0:
+        l = st.sidebar.number_input("l = ", value=1.5, min_value=0.1)
+        models.set_var_math_pendulum(l)
 
-    # equation = equation.replace("a0", str(c[0]))
-    # equation = equation.replace("a1", str(c[1]))
-    # equation = equation.replace("a2", str(c[2]))
-    # equation = equation.replace("a3", str(c[3]))
+    if id_mod == 1:
+        do_m = st.sidebar.number_input("m = ", value=1.0, min_value=0.1, key='kg')
+        do_b = st.sidebar.number_input("damping force = ", value=14.0, min_value=0.1, key='N')
+        do_l = st.sidebar.number_input("streched spring = ", value=0.2, min_value=0.1, key='m')
+        general_solution, s = models.get_damp_osc_analytic(do_m, do_b, do_l)
+        st.sidebar.write(s)
 
     st.sidebar.markdown("***")
     st.sidebar.markdown("Initial conditions")
-    if id_mod < 4:
-        y0 = [st.sidebar.number_input("y(0) = ", value=0.0)]
+    if id_mod < -1:
+        y0 = [st.sidebar.number_input("y(0) = ", value=1.0)]
         init_cond = ("y_{(0)}="+str(y0[0]))
-    elif id_mod == 4:
-        y0 = [st.sidebar.number_input("y'(0) = ", value=0.0),
-              st.sidebar.number_input("y(0) = ", value=0.0)]
-        init_cond = ("y'_{(0)}=" + str(y0[0]) + ",\\;y_{(0)}=" + str(y0[1]))
-    elif id_mod == 5:
-        y0 = [st.sidebar.number_input("y''(0) = ", value=0.0),
-              st.sidebar.number_input("y'(0) = ", value=0.0),
-              st.sidebar.number_input("y(0) = ", value=0.0)]
-        init_cond = ("y''_{(0)}=" + str(y0[0]) + ",\\;y'_{(0)}=" + str(y0[1]) + ",\\;y_{(0)}=" + str(y0[2]))
+    elif id_mod == 0:
+        y0 = [np.radians(st.sidebar.number_input("C1 = ", value=1.0, help="input in °")),
+              np.radians(st.sidebar.number_input("C2= ", value=1.0, help="input in °/s"))]
+        init_cond = ("C1=" + str(y0[0]) + "\\;radians" + ",\\;C2=" + str(y0[1]) + "\\;radians/s")
+        model = models.get_model_str(id_mod)
+    elif id_mod == 1:
+        y0 = [st.sidebar.number_input("C1 = ", value=1.0),
+              st.sidebar.number_input("C2 = ", value=1.0)]
+        init_cond = ("C1=" + str(y0[0]) + ",\\;C2=" + str(y0[1]))
+        model = models.get_model_str(id_mod)
 
     st.sidebar.markdown("***")
     st.sidebar.markdown("RKF45")
-    h_min, h_max = st.sidebar.slider("Size of the step:", 0.0001, 1.000, (0.100, 0.750), 0.001)
-    tol = st.sidebar.slider("Tolerance: (1e(x)):", -12, -2, -5)
+    h_min, h_max = st.sidebar.slider("Size of the step:", 0.1, 1000.0, (100.0, 700.0), 0.1,
+                                     help="Value is divided by 1000")
+    h_min = h_min / 1000
+    h_max = h_max / 1000
+    tol = st.sidebar.slider("Tolerance:", -5, 10, 4, help="Computation method: 1e(-value)")
+    tol = 1 * 10 ** (-tol)
+    st.sidebar.write('step_interval = ' + str([h_min, h_max]))
+    st.sidebar.write('tolerance = ', tol)
 
 with equations:
     st.subheader("Selected Equations")
     left_ode.write("Selected ordinary differential equation:")
-    left_ode.latex(equation)
+    left_ode.latex(model)
+    right_analytic.write("Analytic solution of selected ODE:")
+    # right_analytic.latex(sp.latex("y[1] * cos((mp_g / mp_l) ** (1 / 2) * x) + y[0] * sin((mp_g / mp_l) ** (1 / 2) * x)"))
+    right_analytic.latex('y = C1 \\cos(\\sqrt{\\frac{g}{l}}x) + C2 \\sin(\\sqrt{\\frac{g}{l}}x)')
+
     left_ode.latex(init_cond)
-    right_analytic.write("Analytic solution of selected ODE")
+    left_ode.latex(models.get_const_math_pendulum())
+
+    # l = right_analytic.number_input("l = ", value=1.5, min_value=0.1)
+    # models.set_var_math_pendulum(l)
+
 
 a = start_point_left.number_input("Start of the interval:", value=0)
 b = start_point_right.number_input("End of the interval:", value=10)
 
-# butt_request_a = request_analytical.button('Calculate analytically')
-# butt_request_num = request_numerical.button('Calculate by numerical methods')
-
 with calculations:
-    n = st.slider("Number of steps:", 5, 100, 10)
-    h = (b - a) / n
-    st.write('Step size: ', h)
+    with st.spinner('Calculation in progress...'):
+        if b <= a:
+            st.error('End of the interval has to be higher than start')
+        else:
+            n = st.slider("Number of steps:", 5, 100, 30)
+            h = (b - a) / n
+            st.write('Step size: ', h)
 
-    request_analytical = False
-    request_numerical = False
-    update_requested = False
-    rkf45_status = -1
+            update_requested = False
+            rkf45_status = -1
 
-    analytic = calc_analytical(a, b, f, y0)
+            analytic = calc_analytical_chart(a, b, general_solution, y0, 0.01)
 
-    if st.button('Calculate by numerical methods'):
-        numeric, rkf45, rkf45_status = calc_numerical(f, a, b, y0, n, h, h_min, h_max, tol)
-        update_requested = True
+            if st.button('Calculate by numerical methods'):
+                numeric, rkf45, rkf45_status = calc_numerical(f, a, b, y0, n, h, h_min, h_max, tol)
+                update_requested = True
 
-    get_chart(numeric, rkf45, rkf45_status, analytic, update_requested)
+            get_chart(numeric, rkf45, rkf45_status, analytic, update_requested)
 
-    st.text("Note: You can select methods in the legend (for multiple selection hold SHIFT and click)")
-    if -1 == rkf45_status:
-        st.warning("RKF45 could not converge to the required tolerance with chose minimum step size, please adjust"
-                   " the parameters.")
+            st.text("Note: You can select methods in the legend (for multiple selection hold SHIFT and click)")
+            if -1 == rkf45_status:
+                st.warning("RKF45 could not converge to the required tolerance with chose minimum step size, please adjust"
+                           " the parameters.")
 
-    with st.beta_expander('Show me the values in the table'):
-        st.write(numeric)
+            table = get_table(numeric, calc_analytical_chart(a, b, general_solution, y0, h))
 
-# with about:
-#     st.__version__
+            if update_requested:
+                st.markdown("***")
+                st.write(table)
+                st.subheader("Global Truncation")
+                calc_truncation(table)
+
+
+with footer:
+    st.markdown('***')
+    st.subheader("Useful links:")
+    st.markdown(link, unsafe_allow_html=True)
+    st.write("Streamlit version: " + st.__version__)
--- models.py
+++ models.py
@@ -1,49 +1,166 @@
-from math import sin
+from math import sin, cos, exp
+import numpy as np
 
 #   purpose of this module is provide defined models of the systems of Ordinary Differential Equations
 
+# global vars for math pendulum
+mp_g = 9.81
+mp_l = 1.5
 
-def get_model_str():
-    str_f = [""] * 6
-    str_f[0] = "y' = y"
-    str_f[1] = "y' = 20x -20y +21"
-    str_f[2] = "y' = x^2 + 1"
-    str_f[3] = "y' = y - x^2 + 1"
-    str_f[4] = "y'' = -y + 4x + 10*sin(x)"
-    str_f[5] = "y'''  = -4y'' + 2y' - y = 0"
-    return str_f
+# global vars for dumped oscilations
+do_roots = [0] * 2
+k = 0.20
+do_g = 9.81
+do_m = 1   # mass
+do_b = 14   # damping force
+do_l = 0.20   # spring [m]
 
-
-def f0(x, y):
-    return y[0]
-
-
-def f1(x, y):
-    return 20 * x - 20 * y[0] + 21
+formula_a = ""
 
 
-def f2(x, y):
-    return 1 + x**2 + 0 * y[0]
+def get_model_str(id):
+    model = ""
+    if id == 0:
+        model = "y'' = -\\frac{g}{l}\\sin(y)"
+    if id == 1:
+        model = "y'' = - by' -\\frac{mg}{l}y = 0"
+    return model
 
 
-def f3(x, y):
-    return y[0] - x**2 + 1
+def get_system_str():
+    model = [""] * 2
+    model[0] = "Mathematical pendulum"
+    model[1] = "Damped oscillations"
+    return model
 
 
-# y'' + y = 4x + 10*sin(x)
-# y' = y'
-# y'' = 4x + 10sin(x) - y
-def f4(x, y):
-    return y[1], 4 * x + 10 * sin(x) - y[0]
+# FUNCTIONS FOR MATHEMATICAL PENDULUM
+def model_math_pendulum(x, y):
+    global mp_g
+    global mp_l
+    return y[1], -(mp_g / mp_l) * sin(y[0])
 
 
-# y''' + 4y'' + 2y' - y = 0
-# y' = y'
-# y'' = y''
-# y''' = -4y'' -2y' +y
-def f5(x, y):
-    return y[2] + 0 * x, y[1], - 4 * y[2] - 2 * y[1] - y[0] + 0 * x
+def analytic_math_pendulum(x, y):
+    return y[0] * cos((mp_g / mp_l) ** (1 / 2) * x) + y[1] * sin((mp_g / mp_l) ** (1 / 2) * x)
 
+
+def get_const_math_pendulum():
+    global mp_g
+    global mp_l
+    constants = "g = " + str(mp_g) + "\\;\\frac{N}{kg} " + ",\\;l = " + str(mp_l) + "\\;m\\;"
+    return constants
+
+
+def set_var_math_pendulum(l):
+    global mp_l
+    mp_l = l
+    return
+
+
+# FUNCTIONS FOR DAMPED OSCILLATION
+# my'' + do_b y' + 49'y = 0
+def model_do(x, y):
+    global do_l
+    global do_m
+    global do_g
+    global do_b
+    return y[1], (-y[1] * do_b - (do_m * do_g)/do_l * y[0]) / do_m
+
+
+def get_damp_osc_analytic(m, b, l):
+    global do_roots
+    global do_m
+    global do_b
+    global do_l
+    global formula_a
+
+    poly = np.poly1d([m, b, (m * do_g) / l])
+    r = poly.r
+
+    if not(any(np.iscomplex(r))):
+        f = get_analytical_real
+        s = ''
+    elif any(np.iscomplex(r)):
+        f = get_analytical_img
+        s = 'img'
+    else:
+        f = get_analytical_equal
+        s = 'equal'
+
+    do_roots = r
+    do_m = m
+    do_b = b
+    do_l = l
+    return f, s
+
+
+def get_analytical_real(x, y):
+    global do_roots
+    return y[0] * exp(do_roots[0] * x) + y[1] * exp(do_roots[1] * x)
+
+
+def get_analytical_img(x, y):
+    global do_roots
+    return exp(do_roots[0].real * x) * (y[0] * cos(do_roots[0].imag * x) + y[1] * sin(do_roots[0].imag * x))
+
+
+def get_analytical_equal(x, y):
+    global do_roots
+    return exp(-do_roots[0] * x) * (y[0] + y[1] * x)
+
+
+def get_do_constants():
+    global do_l
+    global do_m
+    global do_g
+    global do_b
+    constants = ("l = " + str(do_l) + "\\;\\m " + ",\\;m = " + str(do_m) + "\\;kg\\;g = " + str(do_g)
+                 + "\\;\\frac{N}{kg} " + ",\\;b = " + str(do_b) + "\\;N\\;")
+    return
+
+
+# def get_
+
+
+# def set_var_throw_motion(l):
+#     global mp_l
+#     mp_l = l
+#     return
+
+# def f0(x, y):
+#     return y[0]
+#
+#
+# def f1(x, y):
+#     return 20 * x - 20 * y[0] + 21
+#
+#
+# def f2(x, y):
+#     return 1 + x**2 + 0 * y[0]
+#
+#
+# def f3(x, y):
+#     return y[0] - x**2 + 1
+#
+#
+# # y'' + y = 4x + 10*sin(x)
+# # y' = y'
+# # y'' = 4x + 10sin(x) - y
+# def f4(x, y):
+#     return y[1], 4 * x + 10 * sin(x) - y[0]
+#
+#
+# # y''' + 4y'' + 2y' - y = 0
+# # y' = y'
+# # y'' = y''
+# # y''' = -4y'' -2y' +y
+# def f5(x, y):
+#     return y[2] + 0 * x, y[1], - 4 * y[2] - 2 * y[1] - y[0] + 0 * x
+
+
+
+# ############################          CHANGED
 
 # C = tmp = np.empty(4, float)
 #
